ce0c8ba87d77d895f75443c169984b70f74c3143
jc-chevrier <47176536+jc-chevrier@users.noreply.github.com>
2021-11-30 17:58:18 +0100

    Init.

---
+++ src/main/java/fr/ul/miage/chevrier/banque/Application.java
@@ -0,0 +1,11 @@
+package fr.ul.miage.chevrier.banque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
