4f3785ebc3d56b25e153ecd030d7e07b7b0dbae5
jc-chevrier <47176536+jc-chevrier@users.noreply.github.com>
2022-01-17 15:11:47 +0100

    Correctifs.

--- target/generated-sources/annotations/fr/ul/miage/chevrier/dbank_api/mapper/CardMapperImpl.java
+++ target/generated-sources/annotations/fr/ul/miage/chevrier/dbank_api/mapper/CardMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-01-17T02:25:00+0100",
+    date = "2022-01-17T15:11:28+0100",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
